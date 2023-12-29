af05b6c9adf07f4bf406b31fbab7a22f88ee3bb9
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-29 16:37:06 -0300

    Add necessary configuration files and initial code

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black min-h-screen w-screen font-bold text-white flex items-center justify-center">
+      test
+    </div>
+  );
+}
+
+export default App;
